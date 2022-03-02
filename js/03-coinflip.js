99cd1210804ee1531ad248703ca40252e748bfe2
SoapyHoang <sxhoang@ucsd.edu>
2022-03-01 19:50:32 -0800

    Made prompt to be case-insensitive

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -6,20 +6,22 @@ console.log(randomNum)
 
 let choice = prompt('Please select "Heads" or "Tails"')
 
-if (randomNum <= 50) {
-    console.log('Heads')
-        if (choice == 'Heads') {
-            alert('The flip was heads and you chose heads...you win!')
-        } else if (choice == 'Tails') {
-            alert('The flip was heads but you chose tails...you lose!')
-        }
-}
-
-if (randomNum > 50) {
-    console.log('Tails')
-        if (choice == 'Heads') {
-            alert('The flip was tails but you chose heads...you lose!')
-        } else if (choice == 'Tails') {
-            alert('The flip was tails and you chose tails...you win!')
-        }
+if (choice != null) {
+    if (randomNum <= 50) {
+        console.log('Heads')
+            if (choice.toLowerCase() == 'heads') {
+                alert('The flip was heads and you chose heads...you win!')
+            } else if (choice.toLowerCase() == 'tails') {
+                alert('The flip was heads but you chose tails...you lose!')
+            }
+    }
+    
+    if (randomNum > 50) {
+        console.log('Tails')
+            if (choice.toLowerCase() == 'heads') {
+                alert('The flip was tails but you chose heads...you lose!')
+            } else if (choice.toLowerCase() == 'tails') {
+                alert('The flip was tails and you chose tails...you win!')
+            }
+    }
 }
